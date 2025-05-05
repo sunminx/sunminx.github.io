c3a60593dde44e5f49ed8552e698bd6deea0fc3b
sunminx <2421500571@qq.com>
2025-05-05 17:00:38 +0800

    add: build slef blog by vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,55 @@
+const base = "";
+
+export default {
+  base,
+  // 站点级选项
+  title: "sunmin", // 网站标题
+  description: "sunmin's blog", // 网站描述
+
+  themeConfig: {
+    // 主题级选项
+    nav: [
+      { text: '关于', link: '/about' },
+      {
+        text: '大前端',
+        items: [
+          { text: "html", link: "/bigFrontEnd/html/" },
+          { text: "css", link: "/bigFrontEnd/css/" },
+          { text: "js", link: "/bigFrontEnd/js/" },
+        ]
+      }
+    ],
+    sidebar: {
+      "/bigFrontEnd/html/": {
+        text: "html",
+        items: [
+          { text: "html", link: "/bigFrontEnd/html/" },
+          { text: "html1", link: "/bigFrontEnd/html/html1" },
+          { text: "html2", link: "bigFrontEnd/html/html2" },
+        ],
+      },
+      "/bigFrontEnd/css/": {
+        text: "css",
+        items: [
+          { text: "css1", link: "/bigFrontEnd/css/css1" },
+          { text: "css2", link: "/bigFrontEnd/css/css2" },
+        ],
+      },
+      "/bigFrontEnd/js/": {
+        text: "js",
+        items: [
+          { text: "js1", link: "/bigFrontEnd/js/js1" },
+          { text: "js2", link: "/bigFrontEnd/js/js2" },
+        ],
+      },
+    },
+    docFooter: {
+      prev: "上一篇",
+      next: "下一篇",
+    },
+  },
+  head: [
+    // 配置网站的图标（显示在浏览器的 tab 上）
+    ["link", { rel: "icon", href: `${base}/favicon.png` }],
+  ],
+};
---
+++ docs/bigFrontEnd/html/index.md
@@ -0,0 +1,6 @@
+# html 专题
+
+## 目录
+
+- [html1](./html1.md)
+- [html2](./html2.md)
